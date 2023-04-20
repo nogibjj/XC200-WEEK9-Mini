55d8d7d5515dc739346ee3b952f33145604cc46f
XiaozheCheng <117330000+XiaozheCheng@users.noreply.github.com>
2023-04-20 14:45:57 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-use rand::Rng;
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    // Generate a random number between 1 and 100
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    print!("Enter a string: ");
+    io::stdout().flush().unwrap();
 
-    println!("Guess the number between 1 and 100!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
 
-    loop {
-        // Get a guess from the user
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-        // Parse the guess as an integer
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        // Check if the guess is correct
-        match guess.cmp(&secret_number) {
-            std::cmp::Ordering::Less => println!("Too small!"),
-            std::cmp::Ordering::Greater => println!("Too big!"),
-            std::cmp::Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    let reversed = input.trim().chars().rev().collect::<String>();
+    println!("Reversed string: {}", reversed);
 }
